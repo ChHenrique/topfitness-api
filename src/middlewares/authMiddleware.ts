448746e1d41520acfe3d4e218140294b5db6c1ee
ChHenrique <ch1claudiohenrique@gmail.com>
2025-07-28 16:14:58 -0300

    fix: authmiddleware commented

--- src/middlewares/authMiddleware.ts
+++ src/middlewares/authMiddleware.ts
@@ -5,9 +5,9 @@ import { ServerError } from "src/services/serverError";
 import { userDTO } from "src/types/user";
 
 export async function authMiddleware(req: FastifyRequest){
-    const token = req.cookies.token;
-    if (!token) throw new ServerError("Unauthorized", 401);
+    // const token = req.cookies.token;
+    // if (!token) throw new ServerError("Unauthorized", 401);
 
-    const decoded = jwt.verify(token, env.JWT_SECRET as string) as userDTO;
-    req.user = decoded;
+    // const decoded = jwt.verify(token, env.JWT_SECRET as string) as userDTO;
+    // req.user = decoded;
 }

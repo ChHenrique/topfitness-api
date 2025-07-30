404b69df3fa17ec0baa5e8af79b300f10d3a10bc
ChHenrique <ch1claudiohenrique@gmail.com>
2025-07-30 15:50:12 -0300

    claudio fixes

--- src/controllers/personal/getAllPersonalController.ts
+++ src/controllers/personal/getAllPersonalController.ts
@@ -5,11 +5,10 @@ import { ServerError } from "src/services/serverError";
 export async function getAllPersonalController(fastify: fastifyContextDTO){
     const user = fastify.req.user;
 
-    // if (!user) throw new ServerError("Usuário não autenticado", 401);
+     if (!user) throw new ServerError("Usuário não autenticado", 401);
     // if (user.role !== "ADMINISTRADOR") throw new ServerError("Acesso negado", 403);
 
     const personals = await getAllPersonals();
-    if (!personals || personals.length === 0) throw new ServerError("Nenhum personal encontrado", 404);
 
     const sanitizedPersonals = personals.map(({ senha, role, ...rest }) => rest);
     fastify.res.status(200).send(sanitizedPersonals);

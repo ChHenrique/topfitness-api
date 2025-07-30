4b4f6b13ef8177b16af2a255ae09b8b793238dbc
ChHenrique <ch1claudiohenrique@gmail.com>
2025-07-30 18:24:54 -0300

    fix: c fies

--- src/controllers/student/deleteStudentController.ts
+++ src/controllers/student/deleteStudentController.ts
@@ -1,10 +1,15 @@
 import { fastifyContextDTO } from "src/interfaces/fastifyContextDTO";
-import { deleteStudent, getStudentById } from "src/services/database/IStudentRepository";
+import { deleteStudent, getStudentByEmail } from "src/services/database/IStudentRepository";
+import { getUserById } from "src/services/database/IUserRepository";
+import { ServerError } from "src/services/serverError";
 import { checkAccessWithPersonal } from "src/utils/checkAccess";
 
 export async function deleteStudentController(fastify: fastifyContextDTO) {
-    const isStudent = await checkAccessWithPersonal(fastify, getStudentById);
+    const isStudent = await checkAccessWithPersonal(fastify, getUserById);
+    const student = await getStudentByEmail(isStudent.email);
 
-    await deleteStudent(isStudent.id);
+    if (!student) throw new ServerError("Aluno não encontrado");
+    await deleteStudent(student.id);
+    
     fastify.res.status(200).send({ message: "Aluno deletado com sucesso" });
 }

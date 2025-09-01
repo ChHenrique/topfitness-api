1f41fd779985477df0ecbdbe97e6f8c36b7c98b2
ChHenrique <ch1claudiohenrique@gmail.com>
2025-09-01 14:32:34 -0300

    feat: a

--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -7,5 +7,5 @@ import { autoLogin } from "../controllers/auth/autoLogin";
 export async function authRoutes(fastify: FastifyInstance){
     fastify.post("/login", async (req, res) => await loginUserController({req, res}))
     fastify.delete("/logout", { preHandler: authMiddleware}, async (req, res) => await logOutUserController({req, res}))
-    fastify.get("/auto-login", { preHandler: autoLogin }, async (req, res) => await autoLogin(req, res))
+    fastify.get("/auto-login", { preHandler: authMiddleware }, async (req, res) => await autoLogin(req, res))
 }

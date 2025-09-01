e8596403161933297c5747d3b890360d22f5dc59
ChHenrique <ch1claudiohenrique@gmail.com>
2025-09-01 14:18:40 -0300

    feat: a

--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -2,7 +2,7 @@ import { FastifyInstance } from "fastify";
 import { loginUserController } from "../controllers/auth/loginUser";
 import { logOutUserController } from "../controllers/auth/logoutUser";
 import { authMiddleware } from "../middlewares/authMiddleware";
-import { autoLogin } from "src/controllers/auth/autoLogin";
+import { autoLogin } from "../controllers/auth/autoLogin";
 
 export async function authRoutes(fastify: FastifyInstance){
     fastify.post("/login", async (req, res) => await loginUserController({req, res}))
